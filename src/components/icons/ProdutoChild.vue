72f98af850e6543239d2e8dde32b1961a66394aa
anajuliamalfim <anajuliabonfim129@gmail.com>
2026-04-14 16:55:49 -0300

    first commit

---
+++ src/components/icons/ProdutoChild.vue
@@ -0,0 +1,16 @@
+<script setup>
+ defineProps(['nome', 'preco', 'categoria'])
+</script>
+
+<template>
+    <li>
+         {{ produto.nome }}
+        - R$ {{ produto.preco }} 
+        ({{ produto.categoria }})
+    </li>
+
+</template>
+
+<style scoped>
+
+</style>
